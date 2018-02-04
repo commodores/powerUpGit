82ea16d5a029e6467a80110c43ff7af49e36eb65
Darren.Hagler <Darren.Hagler@4CA-L78184.dougherty.k12.ga.us>
2018-02-04 18:26:31 -0500

    Few Changes

--- powerUp/src/org/usfirst/frc6919/powerUp/commands/driveManual.java
+++ powerUp/src/org/usfirst/frc6919/powerUp/commands/driveManual.java
@@ -43,6 +43,7 @@ public class driveManual extends Command {
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
+    	Robot.driveTrain.drive(Robot.oi.gamePad.getRawAxis(1), Robot.oi.gamePad.getRawAxis(5));
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -54,11 +55,13 @@ public class driveManual extends Command {
     // Called once after isFinished returns true
     @Override
     protected void end() {
+    	Robot.driveTrain.stop();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     @Override
     protected void interrupted() {
+    	end();
     }
 }

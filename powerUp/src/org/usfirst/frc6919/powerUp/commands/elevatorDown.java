a9f9d50f6c9f9b28c84e22bcbafbc3ba3e28b719
User <User@10.69.19.149>
2018-02-19 14:53:30 -0500

    new

--- powerUp/src/org/usfirst/frc6919/powerUp/commands/elevatorDown.java
+++ powerUp/src/org/usfirst/frc6919/powerUp/commands/elevatorDown.java
@@ -55,7 +55,7 @@ public class elevatorDown extends Command {
     // Called once after isFinished returns true
     @Override
     protected void end() {
-    	Robot.elevator.elevatorHold();
+    	Robot.elevator.elevatorStop();
     }
 
     // Called when another command which requires one or more of the same

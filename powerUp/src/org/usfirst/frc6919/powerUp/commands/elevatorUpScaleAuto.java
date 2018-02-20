4eb6a9be2e51f6506f08c223cca51d95672a9922
User <User@10.69.19.149>
2018-02-20 08:52:50 -0500

    test

--- powerUp/src/org/usfirst/frc6919/powerUp/commands/elevatorUpScaleAuto.java
+++ powerUp/src/org/usfirst/frc6919/powerUp/commands/elevatorUpScaleAuto.java
@@ -43,22 +43,25 @@ public class elevatorUpScaleAuto extends Command {
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
+    	Robot.elevator.elevatorUp();
     }
 
     // Make this return true when this Command no longer needs to run execute()
     @Override
     protected boolean isFinished() {
-        return false;
+    	return Robot.elevator.getUpLimit();
     }
 
     // Called once after isFinished returns true
     @Override
     protected void end() {
+    	Robot.elevator.elevatorStop();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     @Override
     protected void interrupted() {
+    	end();
     }
 }

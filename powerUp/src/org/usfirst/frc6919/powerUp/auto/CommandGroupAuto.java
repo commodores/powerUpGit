ef720672b12d9b4a1bb4e6bb9a1b185bd89cccbd
Darren.Hagler <Darren.Hagler@4CA-L78184.dougherty.k12.ga.us>
2018-02-26 15:57:40 -0500

    fixed auto

--- powerUp/src/org/usfirst/frc6919/powerUp/auto/CommandGroupAuto.java
+++ powerUp/src/org/usfirst/frc6919/powerUp/auto/CommandGroupAuto.java
@@ -12,159 +12,195 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 /** the sequence of commands for autonomous */
 public class CommandGroupAuto extends CommandGroup {
 
-	//Stores the states of the switches and scale
+	// Stores the states of the switches and scale
 	String gameData;
 
 	public CommandGroupAuto(Position position, Goal goal, Crosser crosser) {
-		//Get the state of the switches and scale for each round
+		// Get the state of the switches and scale for each round
 		gameData = DriverStation.getInstance().getGameSpecificMessage();
 
 		// make sure everything is in the right state/position here
 
 		switch (position) {
-			case LEFT:
-				switch (goal){
-					case CROSS_LINE:
-						addSequential(new autoForward108());
-						break;
-					case SWITCH:
-						switch (crosser) {
-							case DO_NOT_CROSS:
-								if (gameData.charAt(0) == 'L'){
-										//when the switch is on the left
-										addSequential(new autoForward144());
-										addParallel(new turnRight90());
-										addSequential(new elevatorUpSwitchAuto());
-										addSequential(new intakeOutAuto());
-									} else {
-										//When the switch is on the right
-										addSequential(new autoForward108());
-									}
-								break;
-							case CROSS:
-								if (gameData.charAt(0) == 'L'){
-									//when the switch is on the left
-									addSequential(new autoForward144());
-									addParallel(new turnRight90());
-									addSequential(new elevatorUpSwitchAuto());
-									addSequential(new intakeOutAuto());
-								} else {
-									//When the switch is on the right
-									addSequential(new autoForward48());
-									addSequential(new turnRight90());
-									addSequential(new autoFwdFullSpeed216());
-									addParallel(new turnLeft90());
-									addSequential(new elevatorUpSwitchAuto());
-									addSequential(new autoFwdHalfSpeed24());
-									addSequential(new intakeOutAuto());
-								}
-							break;
-						}	
-						break;
-					case SCALE:
-						if (gameData.charAt(1) == 'L'){
-							//when the scale is on the left
-							addSequential(new autoFwdQuarterSpeed24());
-							addSequential(new autoFwdHalfSpeed24());
-							addSequential(new autoFwd3QuarterSpeed24());
-							addParallel(new elevatorUpScaleAuto());
-							addSequential(new autoFwdFullSpeed216());
-							addSequential(new turnRight90());
-							addSequential(new autoReverse12());
-							addSequential(new intakeOutAuto());
-						} else {
-							//When the scale is on the right
-							addSequential(new autoForward108());
-						}
-						break;
+		case LEFT:
+			switch (goal) {
+			case CROSS_LINE:
+				addSequential(new autoFwdCrossLine());
+				break;
+			case SWITCH:
+				switch (crosser) {
+				case DO_NOT_CROSS:
+					if (gameData.charAt(0) == 'L') {
+						// when the switch is on the left
+						addSequential(new autoFwdSwitch());
+						addParallel(new turnRight90());
+						addSequential(new elevatorUpSwitchAuto());
+						addSequential(new intakeOutAuto());
+					} else {
+						// When the switch is on the right
+						addSequential(new autoFwdCrossLine());
+					}
+					break;
+				case CROSS:
+					if (gameData.charAt(0) == 'L') {
+						// when the switch is on the left
+						addSequential(new autoFwdSwitch());
+						addParallel(new turnRight90());
+						addSequential(new elevatorUpSwitchAuto());
+						addSequential(new intakeOutAuto());
+					} else {
+						// When the switch is on the right
+						addSequential(new autoFwdCrossSwitch());
+						addSequential(new turnRight90());
+						addSequential(new autoFwdCrossFieldSwitch());
+						addParallel(new turnLeft90());
+						addSequential(new elevatorUpSwitchAuto());
+						addSequential(new autoForward12());
+						addSequential(new intakeOutAuto());
+					}
+					break;
 				}
 				break;
-
-			case CENTER:
-				switch (goal){
-					case CROSS_LINE:
-						addSequential(new autoForward108());
-						break;
-					case SWITCH:
-						if (gameData.charAt(0) == 'L'){
-							//when the switch is on the left
-							addSequential(new autoForward108());
-						} else {
-							//When the switch is on the right
-							addSequential(new autoForward108());
-							addSequential(new elevatorUpSwitchAuto());
-							addSequential(new intakeOutAuto());
-						}
-						break;
-					case SCALE:
-						if (gameData.charAt(1) == 'L'){
-							//when the scale is on the left
-							addSequential(new autoForward108());
-						} else {
-							//When the scale is on the right
-							addSequential(new autoForward108());
-						}
-						break;
+			case SCALE:
+				switch (crosser) {
+				case DO_NOT_CROSS:
+					if (gameData.charAt(1) == 'L') {
+						// when the scale is on the left
+						addParallel(new elevatorUpScaleAuto());
+						addSequential(new autoFwdScale());
+						addSequential(new turnRight90());
+						addSequential(new intakeOutAuto());
+					} else {
+						// When the scale is on the right
+						addSequential(new autoFwdCrossLine());
+					}
+					break;
+				case CROSS:
+					if (gameData.charAt(1) == 'L') {
+						// when the scale is on the left
+						addParallel(new elevatorUpScaleAuto());
+						addSequential(new autoFwdScale());
+						addSequential(new turnRight90());
+						addSequential(new intakeOutAuto());
+					} else {
+						// When the scale is on the right
+						addSequential(new autoFwdCrossScale());
+						addSequential(new turnRight90());
+						addParallel(new elevatorUpScaleAuto());
+						addSequential(new autoFwdCrossFieldScale());
+						addSequential(new turnLeft90());
+						addSequential(new autoForward12());
+						addSequential(new intakeOutAuto());
+					}
+					break;
+				}
+				break;
 			}
+			break;
+
+		case CENTER:
+			switch (goal) {
+			case CROSS_LINE:
+				addSequential(new autoFwdCrossLine());
 				break;
-				
-			case RIGHT:
-				switch (goal){
-					case CROSS_LINE:
-						addSequential(new autoForward108());
-						break;
-					case SWITCH:
-						switch (crosser) {
-							case DO_NOT_CROSS:
-								if (gameData.charAt(0) == 'R'){
-										//when the switch is on the right
-										addSequential(new autoForward144());
-										addParallel(new turnLeft90());
-										addSequential(new elevatorUpSwitchAuto());
-										addSequential(new intakeOutAuto());
-									} else {
-										//When the switch is on the right
-										addSequential(new autoForward108());
-									}
-								break;
-							case CROSS:
-								if (gameData.charAt(0) == 'R'){
-									//when the switch is on the left
-									addSequential(new autoForward144());
-									addParallel(new turnLeft90());
-									addSequential(new elevatorUpSwitchAuto());
-									addSequential(new intakeOutAuto());
-								} else {
-									//When the switch is on the right
-									addSequential(new autoForward48());
-									addSequential(new turnLeft90());
-									addSequential(new autoFwdFullSpeed216());
-									addParallel(new turnRight90());
-									addSequential(new elevatorUpSwitchAuto());
-									addSequential(new autoFwdHalfSpeed24());
-									addSequential(new intakeOutAuto());
-								}
-							break;
-						}	
-						break;
-					case SCALE:
-						if (gameData.charAt(1) == 'L'){
-							//when the scale is on the left
-							addSequential(new autoFwdQuarterSpeed24());
-							addSequential(new autoFwdHalfSpeed24());
-							addSequential(new autoFwd3QuarterSpeed24());
-							addParallel(new elevatorUpScaleAuto());
-							addSequential(new autoFwdFullSpeed216());
-							addSequential(new turnLeft90());
-							addSequential(new autoReverse12());
-							addSequential(new intakeOutAuto());
-						} else {
-							//When the scale is on the right
-							addSequential(new autoForward108());
-						}
-						break;
+			case SWITCH:
+				if (gameData.charAt(0) == 'L') {
+					// when the switch is on the left
+					addSequential(new autoFwdCrossLine());
+				} else {
+					// When the switch is on the right
+					addSequential(new autoFwdCrossLine());
+					addSequential(new elevatorUpSwitchAuto());
+					addSequential(new intakeOutAuto());
 				}
 				break;
-		}	
+			case SCALE:
+				if (gameData.charAt(1) == 'L') {
+					// when the scale is on the left
+					addSequential(new autoFwdCrossLine());
+				} else {
+					// When the scale is on the right
+					addSequential(new autoFwdCrossLine());
+				}
+				break;
+			}
+			break;
+
+		case RIGHT:
+			switch (goal) {
+			case CROSS_LINE:
+				addSequential(new autoFwdCrossLine());
+				break;
+			case SWITCH:
+				switch (crosser) {
+				case DO_NOT_CROSS:
+					if (gameData.charAt(0) == 'R') {
+						// when the switch is on the right
+						addSequential(new autoFwdSwitch());
+						addParallel(new turnLeft90());
+						addSequential(new elevatorUpSwitchAuto());
+						addSequential(new intakeOutAuto());
+					} else {
+						// When the switch is on the left
+						addSequential(new autoFwdCrossLine());
+					}
+					break;
+				case CROSS:
+					if (gameData.charAt(0) == 'R') {
+						// when the switch is on the right
+						addSequential(new autoFwdSwitch());
+						addParallel(new turnLeft90());
+						addSequential(new elevatorUpSwitchAuto());
+						addSequential(new intakeOutAuto());
+					} else {
+						// When the switch is on the left
+						addSequential(new autoFwdCrossSwitch());
+						addSequential(new turnLeft90());
+						addSequential(new autoFwdCrossFieldSwitch());
+						addParallel(new turnRight90());
+						addSequential(new elevatorUpSwitchAuto());
+						addSequential(new autoForward12());
+						addSequential(new intakeOutAuto());
+					}
+					break;
+				}
+				break;
+			case SCALE:
+				switch (crosser) {
+				case DO_NOT_CROSS:
+					if (gameData.charAt(1) == 'R') {
+						// when the scale is on the right
+						addParallel(new elevatorUpScaleAuto());
+						addSequential(new autoFwdScale());
+						addSequential(new turnLeft90());
+						addSequential(new intakeOutAuto());
+					} else {
+						// When the scale is on the left
+						addSequential(new autoFwdCrossLine());
+					}
+					break;
+				case CROSS:
+					if (gameData.charAt(1) == 'R') {
+						// when the scale is on the right
+						addParallel(new elevatorUpScaleAuto());
+						addSequential(new autoFwdScale());
+						addSequential(new turnLeft90());
+						addSequential(new intakeOutAuto());
+					} else {
+						// When the scale is on the left
+						addSequential(new autoFwdCrossScale());
+						addSequential(new turnLeft90());
+						addParallel(new elevatorUpScaleAuto());
+						addSequential(new autoFwdCrossFieldScale());
+						addSequential(new turnRight90());
+						addSequential(new autoForward12());
+						addSequential(new intakeOutAuto());
+					}
+					break;
+				}
+				break;
+			}
+			break;
+		}
 	}
 }

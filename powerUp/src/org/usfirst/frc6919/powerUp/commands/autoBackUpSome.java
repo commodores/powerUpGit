a2c34ef40f5fa07aac2663aeeec1d83c35980fd1
User <User@User-PC.dougherty.k12.ga.us>
2018-03-09 15:59:14 -0500

    boop

--- powerUp/src/org/usfirst/frc6919/powerUp/commands/autoBackUpSome.java
+++ powerUp/src/org/usfirst/frc6919/powerUp/commands/autoBackUpSome.java
@@ -48,7 +48,7 @@ public class autoBackUpSome extends Command {
     @Override
     protected void execute() {
     	double pTerm = Robot.driveTrain.driveTrainGain * (0.0 - Robot.driveTrain.getIMUAngle());
-    	Robot.driveTrain.drive(-.8 + pTerm, -.8 - pTerm);
+    	Robot.driveTrain.drive(-.625 + pTerm, -.625 - pTerm);
     }
 
     // Make this return true when this Command no longer needs to run execute()
--- powerUp/src/org/usfirst/frc6919/powerUp/commands/autoFwdRightScale.java
+++ powerUp/src/org/usfirst/frc6919/powerUp/commands/autoFwdRightScale.java
@@ -54,7 +54,7 @@ public class autoFwdRightScale extends Command {
     // Make this return true when this Command no longer needs to run execute()
     @Override
     protected boolean isFinished() {
-        return Robot.driveTrain.getEncoderDistance() > 182 || isTimedOut();
+        return Robot.driveTrain.getEncoderDistance() > 176 || isTimedOut();
     }
 
     // Called once after isFinished returns true

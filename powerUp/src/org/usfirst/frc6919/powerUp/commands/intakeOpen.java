e0ec079c62e50ea92ea926edab7eb31ee0587818
dphag <dphag@DESKTOP-I3ED7LN>
2018-03-29 13:51:24 -0400

    testing servos

--- powerUp/src/org/usfirst/frc6919/powerUp/commands/intakeOpen.java
+++ powerUp/src/org/usfirst/frc6919/powerUp/commands/intakeOpen.java
@@ -38,6 +38,7 @@ public class intakeOpen extends Command {
     // Called just before this Command runs the first time
     @Override
     protected void initialize() {
+    	Robot.intake.intakeOpen();
     }
 
     // Called repeatedly when this Command is scheduled to run
